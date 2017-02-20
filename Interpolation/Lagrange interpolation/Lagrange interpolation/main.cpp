b5067dc321b3223ed5e02ceac2ae2307f7116807
al1a5 <ideolos@gmail.com>
2017-02-20 20:44:32 +0300

    Lagrange interpolation initial commit

---
+++ Interpolation/Lagrange interpolation/Lagrange interpolation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lagrange interpolation
+//
+//  Created by al1as on 20/02/17.
+//  Copyright © 2017 al1as. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
