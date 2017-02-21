259726d5f0bf50205cd7cd76a6e518e9903efefe
al1a5 <ideolos@gmail.com>
2017-02-21 19:11:57 +0300

    Changed single thread version. Will add modulo operation in future.

--- Text processing/Rabin-Karp algorithm/Rabin-Karp algorithm/main.cpp
+++ Text processing/Rabin-Karp algorithm/Rabin-Karp algorithm/main.cpp
@@ -9,38 +9,42 @@
 #include <string>
 #include <vector>
 
-void RabinKarpAlgorithm(std::string s, std::string t) {
-    // считаем все степени p
-    const int p = 31;
-    size_t size = t.length();
-    long long p_pow [size];
-    p_pow[0] = 1;
-    for (size_t i=1; i<size; ++i)
-        p_pow[i] = p_pow[i-1] * p;
+long long d = 1;
+
+inline long long rehash(char a, char b, long long h) {
+    long long result = (( h - a * d ) << 1 ) + b;
+    return result;
+}
+
+void RabinKarpAlgorithm(const char *t, const char *s, size_t n, size_t m) {
+    /*
+     Here all the module multiplications by 2 are made using shifts.
+     So q = max integer available
+     */
+    long long hy, hx;
+    size_t i;
+    /*
+     Preprocessing
+     computes d = 2^( m-1 ) with the left-shift operator
+     */
+    for ( i = 1; i < m; i++ ) d = ( d << 1 );
     
-    // считаем хэши от всех префиксов строки T
-    long long h [size];
-    for (size_t i=0; i<t.length(); ++i)
-    {
-        h[i] = (t[i] - 'a' + 1) * p_pow[i];
-        if (i)  h[i] += h[i-1];
+    hy = hx = 0;
+    for ( i = 0; i < m; i++) {
+        hx = ( ( hx << 1 ) + s[i] );
+        hy = ( ( hy << 1 ) + t[i] );
     }
     
-    // считаем хэш от строки S
-    long long h_s = 0;
-    for (size_t i=0; i<s.length(); ++i)
-        h_s += (s[i] - 'a' + 1) * p_pow[i];
+    /* Searching */
     
-    // перебираем все подстроки T длины |S| и сравниваем их
-    for (size_t i = 0; i + s.length() - 1 < t.length(); ++i)
-    {
-        long long cur_h = h[i+s.length()-1];
-        if (i)  cur_h -= h[i-1];
-        // приводим хэши к одной степени и сравниваем
-        if (cur_h == h_s * p_pow[i])
-            std::cout << i << ' ';
+    for ( i=m; i < n; i++ ) {
+        if ( hy == hx && memcmp( &t[ i-m ], s, m ) == 0 )
+            std::cout << ( i-m ) << " ";
+        hy = rehash(t[i-m], t[i], hy);
     }
 }
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -50,6 +54,6 @@ int main(int argc, const char * argv[]) {
     std::cout << "Enter string S:";
     std::cin >> s;
     std::cout << "Rabin-Karp algorithm results:\n";
-    RabinKarpAlgorithm(s, t);
+    RabinKarpAlgorithm(t.c_str(), s.c_str(), t.size()+1, s.size());
     return 0;
 }
