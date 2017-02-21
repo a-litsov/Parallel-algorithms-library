46cb8dfe1220109fd8bd158d199e248aab402ffd
al1a5 <ideolos@gmail.com>
2017-02-21 17:24:04 +0300

    Rabin -Karp text processing algorithm initial commit

---
+++ Interpolation/Lagrange interpolation/build/Lagrange interpolation.build/Release/Lagrange interpolation.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/al1as/Developer/Parallel\ algorithms\ library/Interpolation/Lagrange\ interpolation/Lagrange\ interpolation/main.cpp
---
+++ Text processing/Rabin-Karp algorithm/Rabin-Karp algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Rabin-Karp algorithm
+//
+//  Created by al1as on 21/02/17.
+//  Copyright © 2017 al1as. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
