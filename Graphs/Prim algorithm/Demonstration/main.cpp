ad0f36b308763f4d4271ca78068877b0589dbb27
alitsov <ideolos@gmail.com>
2017-03-30 22:20:03 +0300

    Updated Prim's algorithm project to llvm 4.0

--- Graphs/Prim algorithm/Demonstration/main.cpp
+++ Graphs/Prim algorithm/Demonstration/main.cpp
@@ -14,6 +14,7 @@
 #include <map>
 #include <set>
 #include <list>
+#include <omp.h>
 
 using namespace std;
 
@@ -22,7 +23,8 @@ const int INF = 1000000000; // значение "бесконечность"
 int PrimAlgorithm(int n, int** g) {
     int res = 0;
     vector<bool> used (n);
-    vector<int> min_e (n, INF), sel_e (n, -1);
+    vector<int> min_e (n, INF); // min_e[i] - вес наименьшего допустимого ребра из вершины i
+    vector<int> sel_e (n, -1); // sel_e[i] - конец этого наименьшего ребра
     min_e[0] = 0;
     for (int i=0; i<n; ++i) {
         int v = -1;
@@ -35,11 +37,10 @@ int PrimAlgorithm(int n, int** g) {
         }
         
         used[v] = true;
-        if (sel_e[v] != -1) {
-            cout << v << " " << sel_e[v] << endl;
+        if (sel_e[v] != -1)
             res += g[v][sel_e[v]];
-        }
         
+#pragma omp parallel for
         for (int to=0; to<n; ++to)
             if (g[v][to] < min_e[to]) {
                 min_e[to] = g[v][to];
